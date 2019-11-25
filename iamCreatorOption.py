fc48f1e81f104e6bd64de02cd7e56e3d3ae1af19
ctye85 <christopher.tye@tri-ad.global>
2019-11-25 16:03:07 +0900

    MVP + Option 1 of 2

---
+++ iamCreatorOption.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import sys
+import boto3
+iam = boto3.client('iam')
+
+def makeuser():
+    for response in range(int(input("How many users do you want to create?: "))):
+        # create user
+        response = iam.create_user(
+            UserName=input()
+        )
+        print(response)
+makeuser()
